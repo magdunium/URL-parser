59eef77d73a16c4bde8ef23069d527eaf36c3a24
magdunium <magdunium@gmail.com>
2014-10-08 21:53:35 +0200

    Update code.rb

--- code.rb
+++ code.rb
@@ -3,11 +3,13 @@ file_in = File.open('/file/path', 'r+')
 
 empt_arr = []
   
+extensions = ["co", "com"]
+  
 file_in.each {
     |element| 
     a = element.split("/")[2]
     b = a.split(".")
-      if b[-2] == "co" or b[-2] == "com"
+      if extensions.include?(b[-2])
         b = b[-3..-1].join(".")
       else
         b = b[-2..-1].join(".")
@@ -16,3 +18,10 @@ file_in.each {
   }
 
 empt_arr.uniq!
+
+file_out = File.open('/file/path', 'w+')
+
+empt_arr.each {
+ |line|
+ file_out << line + "\n"
+}
